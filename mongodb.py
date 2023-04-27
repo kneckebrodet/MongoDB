4996dc237951e1f1c35aa9cd04172ea7edd3f529
kneckebrodet <113566464+kneckebrodet@users.noreply.github.com>
2023-04-27 17:29:31 +0900

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -8,7 +8,9 @@ class MongoDB:
 
     def connect_to_localhost(self, database_name="localhost", collection_name=27017):
         self.client = MongoClient(database_name, collection_name)
+        # Choose database
         self.db = self.client.ShootingGame
+        # Choose collection
         self.collection = self.db.Players
 
     def add_new_player(self, player_data):
@@ -16,10 +18,17 @@ class MongoDB:
 
     def get_list_of_players(self):
         playersObject = self.collection.find({}, {"_id": 0})
+        # Convert object file to list of dicts
         rankedlist = [player for player in playersObject]
+        # Convert list of dicts to list of lists
         upd_list = []
         for obj in rankedlist:
             key, value = obj.popitem()
             upd_list.append([key,value])
-
+        
         return upd_list
+    
+mongo = mongodb.MongoDB()
+mongo.connect_to_localhost()
+mongo.add_new_player("player_data")
+list_of_players = mongo.get_list_of_players()
